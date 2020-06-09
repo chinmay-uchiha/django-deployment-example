d2baefb70cc715560e1c5afc32a5f5b5df6b1be0
chinmay-uchiha <awesomeb365@gmail.com>
2020-06-09 15:58:05 +0530

    'first'

--- DemoProject/DemoApp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from DemoApp.models import Student,Subject
-
-class Student_form(forms.ModelForm):
-    class Meta:
-        model=Student
-        fields='__all__'
--- DemoProject/DemoApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from DemoApp import views
-app_name='DemoApp'
-urlpatterns=[
-    path('staff/',views.staff,name='staff'),
-    path('student/',views.student,name='student'),
-    path('student_display/',views.student_display,name='student_display'),
-]
